9d922dffbe93e9ab3c95b68ed4b93bd9da69e53b
kamataryo <mugil.cephalus+github@gmail.com>
2024-11-08 20:24:13 +0900

    fix

--- src/components/viewers/openseadragon.tsx
+++ src/components/viewers/openseadragon.tsx
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { useEffect, useState } from 'react'
 
 type Props = {
   manifest: string
 }
+
+class IIIFViewerOpenseadragonError extends Error {}
+
 export const OpenSeadragonContainer: React.FC<Props> = (props) => {
   const [osdInit, setOsdInit] = useState(false)
-  const osdContainerRef = useRef<HTMLDivElement>(null)
-
 
   const { manifest } = props
 
@@ -17,18 +18,28 @@ export const OpenSeadragonContainer: React.FC<Props> = (props) => {
 
       Promise.all([
         fetch(manifest)
-          .then((resp) => resp.json())
-          .then(manifestObject => manifestObject.items[0].items[0].items[0].body.service.id + '/info.json')
-          .then(imageInfo => fetch(imageInfo))
+          .then((resp) => {
+            return resp.json()
+          })
+          .then(manifestObject => {
+            if(manifestObject['@context'] === 'http://iiif.io/api/presentation/2/context.json') {
+              throw new IIIFViewerOpenseadragonError('IIIF Presentation 2.0 is not supported on this service.')
+            } else {
+              return manifestObject.items[0].items[0].items[0].body.service[0].id + '/info.json'
+            }
+          })
+          .then(imageInfo => {
+            console.log(imageInfo)
+            return fetch(imageInfo)
+          })
           .then(resp => resp.json()),
         import('openseadragon'),
       ])
         .then(([tileSource, OpenSeadragon]) => {
-          if(!osdContainerRef.current) throw new Error('osdContainerRef is not ready')
           OpenSeadragon.default({
-              id: osdContainerRef.current.id,
+              id: 'osd',
               preserveViewport: true,
-              prefixUrl: '/public/openseadragon/images/',
+              prefixUrl: './openseadragon/images/',
               visibilityRatio:    1,
               minZoomLevel:       1,
               defaultZoomLevel:   1,
@@ -37,10 +48,20 @@ export const OpenSeadragonContainer: React.FC<Props> = (props) => {
           });
           setOsdInit(true)
         })
+        .catch(error => {
+          if(error instanceof IIIFViewerOpenseadragonError) {
+            alert(error.message)
+          } else {
+            console.error(error)
+            alert('Unable to load manifest')
+          }
+        })
+
     }
   }, [manifest, osdInit])
 
+
   return (
-    <div className="osd viewer-container" id="osd" ref={osdContainerRef}></div>
+    <div className="osd viewer-container" id="osd"></div>
   )
 }
